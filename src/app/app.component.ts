6902ba7a134c17368822c1f9bf2916da2fe470d3
jan316 <fototyll@gmail.com>
2022-06-14 22:52:46 +0200

    minor code adjustements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,18 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-
-
-
 export class AppComponent implements OnInit {
-
-  arr: Array<number> = [0,0,0];
+  arr: Array<number> = [0, 0, 0];
   count: number = 0;
   score: number = 0;
   a: number = 0;
   b: number = 0;
   c: number = 0;
-
 
   actors = [
     {
@@ -73,12 +68,10 @@ export class AppComponent implements OnInit {
 
   onGoClick() {
     let arr = [];
-    while (arr.length < 3)
-    {
+    while (arr.length < 3) {
       let r = Math.floor(Math.random() * this.actors.length - 1) + 1;
       if (arr.indexOf(r) === -1) arr.push(r);
     }
-
 
     this.a = arr[0];
     this.b = arr[1];
@@ -88,23 +81,18 @@ export class AppComponent implements OnInit {
 
     let ul: any = document.querySelector('ul');
     for (let i = ul.children.length; i >= 0; i--) {
-      ul.appendChild(ul.children[Math.random() * i | 0]);
+      ul.appendChild(ul.children[(Math.random() * i) | 0]);
     }
 
-    this.count = this.count+1;
+    this.count = this.count + 1;
     console.log(this.count);
-
-
-
-
   }
 
   ngOnInit() {
-    this.onGoClick()
+    this.onGoClick();
   }
 
   onRestartClick() {
-    window.location.reload()
+    window.location.reload();
   }
-
 }
