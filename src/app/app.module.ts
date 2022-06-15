c6383ba1b0cc4c1e16c7585814ecb3fdf66a6728
jan316 <fototyll@gmail.com>
2022-06-15 23:59:51 +0200

    work on servis to inject actors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule, routingComponents } from './app-routing/app-routing.module';
+import {
+  AppRoutingModule,
+  routingComponents,
+} from './app-routing/app-routing.module';
+import { ActorsService } from './actors.service';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   declarations: [AppComponent, routingComponents],
+  providers: [ActorsService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
